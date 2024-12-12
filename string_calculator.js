b66996e217d57d129468e6e4645beda4b612e101
anand_shukla_smc <anandshukla@smcindiaonline.com>
2024-12-12 19:09:45 +0530

    For Write Unit Test Cases

--- string_calculator.js
+++ string_calculator.js
@@ -1,3 +1,7 @@
+const express= require('express');
+const mocha= require('mocha');
+const app= express();
+const PORT=process.env.PORT || 3000
 
 console.log("Assement Starts");
 // Creating a function to calculate strings and write Test cases
@@ -22,3 +26,7 @@ console.log("response_1: ",response_1);
 
 
 console.log("Assement Ends");
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`);
+})
